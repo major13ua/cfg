d7239861a52710d8028b47d02db94f8543ed8229
I. Chupryna <ichupryna@gmail.com>
2016-08-13 15:54:09 +0300

    Adding Ruby, Compass install

--- bash/install/generalInstall.sh
+++ bash/install/generalInstall.sh
@@ -2,3 +2,9 @@
 
 sudo apt install software-properties-common
 sudo apt install python-software-properties
+
+sudo apt install ruby-full
+sudo gem update --system
+sudo apt-get install ruby-ffi
+sudo gem install compass
+
